4bf61d565ae2032a129d84f90a0f945bb3aac3dd
Administrator <admin@example.com>
2026-07-26 17:31:02 +0800

    修改magisk配置

--- mode/customize.sh
+++ mode/customize.sh
@@ -3,9 +3,6 @@ sh $MODPATH/vtools/init_vtools.sh $(realpath $MODPATH/module.prop)
 }
 enforce_install_from_magisk_app(){
 echo "欢迎使用finalizer"
-echo "感谢:"
-echo "CookApk@:魔威"
-echo "提供的技术支持"
 }
 Init
 enforce_install_from_magisk_app
